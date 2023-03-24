518195325c362a1cc8784f9fe5d6c49df41c591b
shiqstone <shiqstone@gmail.com>
2023-03-24 23:50:36 +0800

    add common function

--- common.py
+++ common.py
@@ -6,6 +6,9 @@ def step_function(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
 def relu(x):
     return np.maximum(0, x)
 
@@ -30,3 +33,22 @@ def cross_entropy_error(y, t):
     batch_size = y.shape[0]
     delta = 1e-7
     return -np.sum(t * np.log(y+delta)) / batch_size
+
+def numerical_gradient(f, x):
+    h = 1e-4 #0.0001
+    grad = np.zeros_like(x) # 生成和x形状相同的数组
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h)的计算
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h)的计算
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val # 还原值
+    
+    return grad
